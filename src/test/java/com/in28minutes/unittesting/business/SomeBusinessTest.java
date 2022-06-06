15c60a55253b11f48faca586ec0ace5d4cabc360
Pedro Ferreira <pedroferreiracjr@gmail.com>
2022-06-06 17:03:25 -0300

    initial commit

---
+++ src/test/java/com/in28minutes/unittesting/business/SomeBusinessTest.java
@@ -0,0 +1,35 @@
+package com.in28minutes.unittesting.business;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class SomeBusinessTest {
+
+	@Test
+	public void calculateSum_basic() {
+		SomeBusinessImpl business = new SomeBusinessImpl();
+		int actualResult = business.calculateSum(new int[] { 1, 2, 3 });
+		int expectedResult = 6;
+
+		assertEquals(expectedResult, actualResult);
+	}
+
+	@Test
+	public void calculateSum_emptyArray() {
+		SomeBusinessImpl business = new SomeBusinessImpl();
+		int actualResult = business.calculateSum(new int[] {});
+		int expectedResult = 0;
+
+		assertEquals(expectedResult, actualResult);
+	}
+
+	@Test
+	public void calculateSum_onValue() {
+		SomeBusinessImpl business = new SomeBusinessImpl();
+		int actualResult = business.calculateSum(new int[] { 10 });
+		int expectedResult = 10;
+
+		assertEquals(expectedResult, actualResult);
+	}
+}
