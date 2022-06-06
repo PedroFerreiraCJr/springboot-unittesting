364101b947d737e4b83a6567eb3e97f9133ac8fa
Pedro Ferreira <pedroferreiracjr@gmail.com>
2022-06-06 19:06:11 -0300

    refatorando o teste usando as anotações do mockito

--- src/test/java/com/in28minutes/unittesting/business/SomeBusinessMockTest.java
+++ src/test/java/com/in28minutes/unittesting/business/SomeBusinessMockTest.java
@@ -1,23 +1,29 @@
 package com.in28minutes.unittesting.business;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.in28minutes.unittesting.data.SomeDataService;
 
+/**
+ * Como estou usando a versão do JUnit 5, nesta aula é preciso utilizar a
+ * anotação abaixo ao invés da @RunWith(MockitoJUnitRunner.class), que deve ser
+ * usada quando utilizando outro runner no JUnit 4.
+ */
+@ExtendWith(MockitoExtension.class)
 class SomeBusinessMockTest {
 
-	SomeBusinessImpl business = new SomeBusinessImpl();
-	SomeDataService dataServiceMock = mock(SomeDataService.class);
+	@InjectMocks	// Esta anotação cria a instância deste objeto, e injeta os mocks declarados neste teste
+	SomeBusinessImpl business;
 
-	@BeforeEach
-	public void before() {
-		business.setSomeDataService(dataServiceMock);
-	}
+	@Mock			// Esta anotação declara que esse atributo é um mock a ser injetado no objeto de teste
+	SomeDataService dataServiceMock;
 
 	@Test
 	public void calculateSumUsingDataService_basic() {
