c4ec3403c167178e05de58653e12c29a5d1eea18
Pedro Ferreira <pedroferreiracjr@gmail.com>
2022-06-07 12:02:59 -0300

    capturando multiplos valores com argument captor

--- src/test/java/com/in28minutes/unittesting/business/ListMockTest.java
+++ src/test/java/com/in28minutes/unittesting/business/ListMockTest.java
@@ -74,4 +74,21 @@ class ListMockTest {
 		verify(mock).add(captor.capture());
 		assertEquals("SomeString", captor.getValue());
 	}
+
+	@Test
+	public void multipleArgumentCapturing() {
+		// ação
+		mock.add("SomeString1");
+		mock.add("SomeString2");
+
+		// verificação
+		ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+		verify(mock, times(2)).add(captor.capture());
+
+		List<String> allValues = captor.getAllValues();
+
+		assertEquals("SomeString1", allValues.get(0));
+		assertEquals("SomeString2", allValues.get(1));
+
+	}
 }
