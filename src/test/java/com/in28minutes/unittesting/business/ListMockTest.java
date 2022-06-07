ff655c1a27299ca797a53ebdf4865ba76de7bcdd
Pedro Ferreira <pedroferreiracjr@gmail.com>
2022-06-07 11:23:25 -0300

    usando argument matchers

--- src/test/java/com/in28minutes/unittesting/business/ListMockTest.java
+++ src/test/java/com/in28minutes/unittesting/business/ListMockTest.java
@@ -3,6 +3,7 @@ package com.in28minutes.unittesting.business;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.anyInt;
 
 import java.util.List;
 
@@ -11,26 +12,34 @@ import org.junit.jupiter.api.Test;
 class ListMockTest {
 
 	List mock = mock(List.class);
-	
+
 	@Test
 	public void size_basic() {
 		when(mock.size()).thenReturn(5);
-		
+
 		assertEquals(5, mock.size());
 	}
-	
+
 	@Test
 	public void returnDifferentValues() {
 		when(mock.size()).thenReturn(5).thenReturn(10);
-		
+
 		assertEquals(5, mock.size());
 		assertEquals(10, mock.size());
 	}
-	
+
 	@Test
 	public void returnWithParameters() {
 		when(mock.get(0)).thenReturn("in28Minutes");
-		
+
+		assertEquals("in28Minutes", mock.get(0));
+		assertEquals(null, mock.get(1));
+	}
+
+	@Test
+	public void returnWithGenericParameters() {
+		when(mock.get(anyInt())).thenReturn("in28Minutes");
+
 		assertEquals("in28Minutes", mock.get(0));
 		assertEquals("in28Minutes", mock.get(1));
 	}
