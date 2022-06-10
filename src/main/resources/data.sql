9a1550a8ad12db44e812fd0e8791a6b74b6c6be0
Pedro Ferreira <pedroferreiracjr@gmail.com>
2022-06-10 11:11:33 -0300

    aula criando um repository de item; criando método no service e controller para retornar os dados do banco relacionados a item

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,6 @@
 INSERT INTO item(id, name, price, quantity)
 	VALUES(10001, 'Item 1', 10, 20);
+INSERT INTO item(id, name, price, quantity)
+	VALUES(10002, 'Item 2', 5, 10);
+INSERT INTO item(id, name, price, quantity)
+	VALUES(10003, 'Item 3', 15, 2);
