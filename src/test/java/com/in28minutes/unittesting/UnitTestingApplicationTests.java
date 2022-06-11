21b7294f4672a72eed2d586501fed0a5f8260c43
Pedro Ferreira <pedroferreiracjr@gmail.com>
2022-06-11 13:37:47 -0300

    aula de configuração de arquivos properties para um teste em particular

--- src/test/java/com/in28minutes/unittesting/UnitTestingApplicationTests.java
+++ src/test/java/com/in28minutes/unittesting/UnitTestingApplicationTests.java
@@ -2,12 +2,13 @@ package com.in28minutes.unittesting;
 
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = { "classpath:test-configuration.properties" })
 class UnitTestingApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
